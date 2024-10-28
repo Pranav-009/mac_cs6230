185d693c3e16a6942466a9d7fef8e9eab43e4afc
Pranav-009 <108786688+Pranav-009@users.noreply.github.com>
2024-10-28 23:22:15 +0530

    Update model_mac.py

--- mac_project/mac/mac_verif/model_mac.py
+++ mac_project/mac/mac_verif/model_mac.py
@@ -1,15 +1,12 @@
-# model for increment alone
-
 import cocotb
 from cocotb_coverage.coverage import *
 
 counter_coverage = coverage_section(
-    CoverPoint('top.increment_di', vname='increment_di', bins = list(range(0,16))),
-    CoverPoint('top.EN_increment', vname='EN_increment', bins = list(range(0,2))),
-    CoverCross('top.cross_cover', items = ['top.increment_di', 'top.EN_increment'])
+    CoverPoint('top.getResult_a', vname='getResult_a', bins = list(range(0,256))),
+    CoverPoint('top.getResult_b', vname='getResult_b', bins = list(range(0,256))),
+    CoverPoint('top.getResult_c', vname='getResult_c', bins = list(range(0,256))),
+    CoverCross('top.cross_cover', items = ['top.getResult_a', 'top.getResult_b', 'top.getResult_c'])
 )
 @counter_coverage
-def model_counter(current_state, EN_increment: int, increment_di: int) -> int:
-    if(EN_increment):
-        return current_state + increment_di
-    return 0
+def model_mac(A, B: int, C: int) -> int:
+    return A*B + C
