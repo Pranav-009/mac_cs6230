c0dfbd7adb51c61f36bad742d53ee247228f5e06
Pranav-009 <108786688+Pranav-009@users.noreply.github.com>
2024-10-28 23:46:11 +0530

    Update test_mac.py

--- mac_project/mac/mac_verif/test_mac.py
+++ mac_project/mac/mac_verif/test_mac.py
@@ -1,6 +1,3 @@
-# This file is public domain, it can be freely copied without restrictions.
-# SPDX-License-Identifier: CC0-1.0
-
 import os
 import random
 from pathlib import Path
@@ -12,57 +9,34 @@ from model_mac import *
 
 @cocotb.test()
 async def test_mac(dut):
-    """Test to check counter"""
-
-    dut.EN_increment.value = 0
-    dut.EN_decrement.value = 0
 
     clock = Clock(dut.CLK, 10, units="us")  # Create a 10us period clock on port clk
     # Start the clock. Start it low to avoid issues on the first RisingEdge
     cocotb.start_soon(clock.start(start_high=False))
-    dut.RST_N.value = 0
-    await RisingEdge(dut.CLK)
-    dut.RST_N.value = 1
-    dut.EN_increment.value = 1
-    dut.increment_di.value = 10
-    dut._log.info('Incrementing')
-    for i in range(0,10):
-        await RisingEdge(dut.CLK)
-        dut._log.info(f'output {int(dut.read.value)}')
-
-    dut._log.info('No change')
-    dut.increment_di.value = 0
-    dut.decrement_dd.value = 0
-    for i in range(0,4):
-        await RisingEdge(dut.CLK)
-        dut._log.info(f'output {int(dut.read.value)}')
-
-    dut.EN_increment.value = 0
-    dut.EN_decrement.value = 1
-    dut.decrement_dd.value = 5
-    dut._log.info('Decrementing')
-    for i in range(0,5):
-        await RisingEdge(dut.CLK)
-        dut._log.info(f'output {int(dut.read.value)}')
+    
 
     ## test using model
     dut.RST_N.value = 0
     await RisingEdge(dut.CLK)
     dut.RST_N.value = 1
-    dut.EN_increment.value = 1
-    dut.increment_di.value = 10
-    dut._log.info('Incrementing')
 
-    for i in range(0, 5):
-        en = int(dut.EN_increment.value)
+    for i in range(0, 100):
+        en = int(dut.getResult_a.value)
         value = int(dut.increment_di.value)
         ini = int(dut.read.value)
+        
+        get_a = random.randint(0,255)
+        get_b = random.randint(0,255)
+        get_c = random.randint(0,255)
+        
+        dut.getResult_a.value = get_a
+        dut.getResult_b.value = get_b
+        dut.getResult_c.value = get_c
 
         await RisingEdge(dut.CLK)
-        dut._log.info(f'output {int(dut.read.value)}')
+        #dut._log.info(f'output {int(dut.read.value)}')
         
-        counter_out = model_counter(ini, en, value)
-        assert int(counter_out) == int(dut.read.value), f'Counter Output Mismatch, Expected = {counter_out} DUT = {int(dut.read.value)}'
+        mac_out = model_mac(get_a, get_b, get_c)
+        assert int(mac_out) == int(dut.getResult.value), f'MAC Output Mismatch, Expected = {mac_out} DUT = {int(dut.getResult.value)}'
     
-    coverage_db.export_to_yaml(filename="coverage_counter.yml")
-
+    coverage_db.export_to_yaml(filename="coverage_mac.yml")
