ad7d4968235c50748532e7999ee3563f54d5ac77
Pranav-009 <108786688+Pranav-009@users.noreply.github.com>
2024-10-28 23:44:58 +0530

    Update model_mac.py

--- mac_project/mac/mac_verif/model_mac.py
+++ mac_project/mac/mac_verif/model_mac.py
@@ -2,9 +2,9 @@ import cocotb
 from cocotb_coverage.coverage import *
 
 counter_coverage = coverage_section(
-    CoverPoint('top.getResult_a', vname='getResult_a', bins = list(range(0,256))),
-    CoverPoint('top.getResult_b', vname='getResult_b', bins = list(range(0,256))),
-    CoverPoint('top.getResult_c', vname='getResult_c', bins = list(range(0,256))),
+    CoverPoint('top.getResult_a', vname='getResult_a', bins = list(range(0,255))),
+    CoverPoint('top.getResult_b', vname='getResult_b', bins = list(range(0,255))),
+    CoverPoint('top.getResult_c', vname='getResult_c', bins = list(range(0,255))),
     CoverCross('top.cross_cover', items = ['top.getResult_a', 'top.getResult_b', 'top.getResult_c'])
 )
 @counter_coverage
